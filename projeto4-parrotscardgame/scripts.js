9999cfacb3065718ca9e0257eede14d21596b104
Dsemedo <dsemedoo@gmail.com>
2022-06-19 18:19:01 -0300

    JS 100%

--- projeto4-parrotscardgame/scripts.js
+++ projeto4-parrotscardgame/scripts.js
@@ -1,4 +1,13 @@
+let todasCartas = ["unicorn", "unicorn", "fiesta", "fiesta", "metal", "metal", "bobross", "bobross", "explody", "explody", "revertit", "revertit", "triplets", "triplets"];
+let algumasCartas = [];
+let carta1;
+let carta2;
+let jogadas = 0;
+let cartaCerta = 0;
+
 let number = prompt("Qual o numero de cartas que deseja jogar? \nSó vale números pares entre 4 e 14!");
+
+//Usuario escolhe o numero de cartas
 
 function par(number){
     if (Number(number) % 2 == 0){
@@ -7,7 +16,7 @@ function par(number){
     return false;
 }
 
-console.log(par(number));
+console.log(par());
 
 function valido (number){
     if (Number(number) >= 4 && Number(number) <= 14){
@@ -16,7 +25,7 @@ function valido (number){
     return false;
 }
 
-console.log(valido(number));
+console.log(valido());
 
 function respostaValida(number){
     return par(number) && valido(number); 
@@ -24,48 +33,93 @@ function respostaValida(number){
 
 console.log(respostaValida());
 
+numeroCartas();
+
 function numeroCartas(){
     while ( number === null || !respostaValida(number)){
         number = prompt("Digite um número par entre 4 e 14, por favor");
     }    
 }
 
-console.log(numeroCartas());
+embaralharCartas();
+//embaralhar as cartas escolhidas pelo usuário 
 
-// criarCartas();
+function embaralharCartas(){
+    for (let i = 0; i < number; i++){
+        const carta = todasCartas[i];
+        algumasCartas.push(carta);
+    }
+    algumasCartas.sort(comparador);
+    adicionarCartas();
+}
 
-// function criarCartas(){
-//     for (let i = 0; i < number; i++){
-//         document.querySelector(".espaco-cartas").innerHTML += `
-//                  <div class="carta">
-//                      <div class="front" onclick="apagarFront(this)">
-//                      </div>         
-//                      <div class="back">
-//                      </div>                     
-//                  </div>`
-//                  ;
-//     }
-// }
+function comparador() {
+    return Math.random() - 0.5;
+  }
 
-const nomes = ["unicorn", "fiesta", "metal", "bobross", "explody", "revertit", "triplets"]
+//
 
-    for (let i=0; i < number; i++){
-         document.querySelector(".espaco-cartas").innerHTML += `
-         <div class="carta">
-             <div class="front" onclick="apagarFront(this)">
-             <img src="Gifs/front.png" alt="" />
-             </div>         
-             <div class="back">
-             <img src="Gifs/${nomes[i]}parrot.gif" alt="" />
-             </div>                     
-         </div>`
-         ;
-    }  
+function adicionarCartas(){
+    for (let i=0; i < algumasCartas.length; i++){
+        document.querySelector(".espaco-cartas").innerHTML += 
+        `
+        <div class="carta" onclick="virarFront(this)">
+            <div class="front-face face">
+            <img src="Gifs/front.png" alt="" />
+            </div>         
 
-function apagarFront(front){
-    const cartaClicada = document.querySelector("virar");
+            <div class="back-face face">
+            <img src="Gifs/${algumasCartas[i]}parrot.gif" alt="" />
+            </div>                     
+        </div>` ;
+   } 
+ }
+
+function virarFront(front){
+    const cartaClicada = document.querySelector("carta");
     if (cartaClicada !== null){
         cartaClicada.classList.remove("virar");
     }
-        front.classList.toggle("virar");
+    front.classList.add("virar");
+
+    jogadas++;
+
+    if (carta1 === undefined){
+        carta1 = front;       
+    } else{
+        carta2 = front;}
+
+        if(carta1.innerHTML == carta2.innerHTML){
+            cartaCerta += 2;
+            resetarCartas();
+        } else{
+            setTimeout(desvirarCartas, 500)
+        }
+
 }
+
+
+function desvirarCartas(){
+    carta1.classList.remove("virar");
+    carta2.classList.remove("virar");
+    resetarCartas();    
+}
+
+console.log(desvirarCartas());
+
+function resetarCartas() {
+    carta1 = undefined;
+    carta2 = undefined;
+    finalizarJogo();
+}
+
+
+console.log(resetarCartas());
+
+function finalizarJogo(){
+    if(cartaCerta == number){
+        setTimeout(alert, 800, `Voce ganhou em ${jogadas/2} jogadas!!`)
+    }
+}
+
+console.log(finalizarJogo());
